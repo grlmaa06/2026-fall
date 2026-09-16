c6266e26a13c8d658da3785a8b52342a387d2a4a
Munkhjargal Manlaibayar <mxmmunkhjargal@gmail.com>
2026-09-16 17:42:20 +0800

    sa/lab1lab1

---
+++ Software-Architecture/lab1/oop-typescript/src/events.ts
@@ -0,0 +1,25 @@
+import { EventEmitter } from 'events';
+import { cart, Item } from './data';
+
+class CartEvents extends EventEmitter {}
+const bus = new CartEvents();
+
+// Хэрэглэх
+bus.on('ITEM_ADDED', (it: Item) => console.log('Нэмэгдсэн:', it.name));
+bus.on('CHECKOUT', (total: number) => console.log('Нийт checkout:', total));
+
+// энгийн cart “модуль”
+let items: Item[] = [...cart];
+
+function addItem(it: Item) {
+    items = [...items, it];
+    bus.emit('ITEM_ADDED', it);
+}
+function checkout() {
+    const total = items.reduce((s, i) => s + i.price * i.qty, 0);
+    bus.emit('CHECKOUT', total);
+    return total;
+}
+// ажиллуулах
+addItem({ name: 'Chocolate', price: 4200, qty: 3, category: 'food' });
+checkout();
